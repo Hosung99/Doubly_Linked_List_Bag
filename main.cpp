72a15940db06d42c385150c83b147392848a2c21
Hosung99 <84187086+Hosung99@users.noreply.github.com>
2021-10-10 16:28:18 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,28 @@
 using namespace std;
 
 int main() {
-	std::cout << "hello" << endl;
 	Bag a;
+	a.insert(3);
+	a.insert(2);
+	a.insert(1);
+	a.sort();
+	a.insert(3);
 	a.insert(5);
-	a.show_contents();
+	a.erase_one(3);
+	a.count(5);
+	a.insert(5);
+	a.count(5);
+	a.count(1);
+
+
+	Bag b(a);
+	b.show_contents();
+
+	Bag c;
+	c += a;
+
+	c.show_contents();
+
+
+	a.sort();
 }
